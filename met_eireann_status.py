3a3dc973ba44ba8ea9dcfb2775ab73130053736b
Sn4u <twalkowiak90317@gmail.com>
2021-04-07 22:43:41 +0100

    Created json files to hold static information. moved creating non-warning cards into own method. Renamed some variables

--- met_eireann_status.py
+++ met_eireann_status.py
@@ -2,62 +2,16 @@ import requests
 import json
 import tkinter
 from tkinter import ttk
-from datetime import *
+from datetime import datetime
 import textwrap
+from os import getcwd
 
-REGION_CODES = {
-    'All counties': 'IRELAND',
-    'Carlow': 'EI01',
-    'Cavan': 'EI02',
-    'Clare': 'EI03',
-    'Cork': 'EI04',
-    'Donegal': 'EI06',
-    'Dublin': 'EI07',
-    'Galway': 'EI10',
-    'Kerry': 'EI11',
-    'Kildare': 'EI12',
-    'Kilkenny': 'EI13',
-    'Leitrim': 'EI14',
-    'Laois': 'EI15',
-    'Limerick': 'EI16',
-    'Longford': 'EI18',
-    'Louth': 'EI19',
-    'Mayo': 'EI20',
-    'Meath': 'EI21',
-    'Monaghan': 'EI22',
-    'Offaly': 'EI23',
-    'Roscommon': 'EI24',
-    'Sligo': 'EI25',
-    'Tipperary': 'EI26',
-    'Waterford': 'EI27',
-    'Westmeath': 'EI29',
-    'Wexford': 'EI30',
-    'Wicklow': 'EI31',
-    'Demo': 'Demo'
-}
-MARINE_CODES = {
-    "Malin-Fair": "EI805",
-    "Fair-Belfast": "EI806",
-    "Belfast-Strang": "EI807",
-    "Strang-Carl": "EI808",
-    "Carling-Howth": "EI809",
-    "Howth-Wicklow": "EI810",
-    "Wicklow-Carns": "EI811",
-    "Carns-Hook": "EI812",
-    "Hook-Dungarvan": "EI813",
-    "Dungarvan- Roches": "EI814",
-    "Roches- Mizen ": "EI815",
-    "Mizen- Valentia": "EI816",
-    "Valentia- Loop": "EI817",
-    "Loop- Slyne": "EI818",
-    "Slyne-Erris ": "EI819",
-    "Erris- Rossan": "EI820",
-    "Rossan-BloodyF": "EI821",
-    "BloodF-Malin": "EI822",
-    "IrishSea-IOM-N": "EI825",
-    "IrishSea-South": "EI823",
-    "IrishSea-IOM-S": "EI824",
-}
+# read json files with
+with open(getcwd() + '\\' + "region_codes.json", "r") as file:
+    REGION_CODES = json.loads(file.read())
+
+with open(getcwd() + '\\' + "marine_codes.json", "r") as file:
+    MARINE_CODES = json.loads(file.read())
 
 dimensions = {"width": "60", "height": "10", }
 style_no_warning = {"foreground": "black", "background": "green", "font": "Helvetica 11 bold"}
@@ -77,6 +31,8 @@ warnings_objects = []  # Warnings class objects
 class Warnings:
     def __init__(self, json_data=None):
         warnings_objects.append(self)
+        self.frame = tkinter.Frame(root, **dimensions)
+        self.frame.pack(fill='both', expand=True)
 
         if json_data:
             print(json_data)
@@ -100,85 +56,70 @@ class Warnings:
             self.style = level_to_style[self.level]
 
             if update_time() != 'expired':
-                self.make(json_data)
-        else:
-            self.make(json_data)
+                self.make()
 
+    def make(self):
 
-    def make(self, json_data):
-        self.frame = tkinter.Frame(root, **dimensions)
-        if json_data is None:
-            self.headline = tkinter.Label(self.frame, style_no_warning,
-                                          text="There are no warnings for the selected region")
-            self.frame.configure(height=10, width=60)
-            self.frame.pack(fill='both', expand=True)
-            self.headline.pack(fill='both')
-        else:
-            self.frame = tkinter.Frame(root, **dimensions)
-            self.frame.pack(fill='both', expand=True)
+        # primary
+        self.primary = tkinter.Frame(self.frame, background=self.level, **dimensions)
+        self.primary.pack(fill='both', side='left')
+        self.primary.bind('<Button-1>', self.display_extra)
 
-            # primary
-            self.primary = tkinter.Frame(self.frame, background=self.level, **dimensions, )
-            self.primary.pack(fill='both', side='left', )
-            self.primary.bind('<Button-1>', self.display_extra)
+        self.headline_label = tkinter.Label(self.primary, self.style, text=self.headline, justify='center',
+                                            wraplength=500, font=('Helvetica', 11, 'bold'), width=55)
+        self.headline_label.pack(side='top')
 
-            self.headline = tkinter.Label(self.primary, self.style, text=self.headline, justify='center',
-                                          wraplength=500,
-                                          font=('Helvetica', 11, 'bold'), width=55)
-            self.headline.pack(side='top')
+        self.primary_info_frame = tkinter.Frame(self.primary, background=self.level)
+        self.certainty_frame = tkinter.Frame(self.primary_info_frame)
+        self.delta_frame = tkinter.Frame(self.primary_info_frame)
+        self.severity_frame = tkinter.Frame(self.primary_info_frame)
+        self.primary_info_frame.pack(side='left')
+        self.certainty_frame.pack(side='left')
+        self.delta_frame.pack(side='left')
+        self.severity_frame.pack(side='left')
 
-            self.primary_info_frame = tkinter.Frame(self.primary, background=self.level)
-            self.certainty_frame = tkinter.Frame(self.primary_info_frame)
-            self.delta_frame = tkinter.Frame(self.primary_info_frame)
-            self.severity_frame = tkinter.Frame(self.primary_info_frame)
-            self.primary_info_frame.pack(side='left')
-            self.certainty_frame.pack(side='left')
-            self.delta_frame.pack(side='left')
-            self.severity_frame.pack(side='left')
+        self.certainty_label = tkinter.Label(self.certainty_frame, self.style, text="Certainty: ")
+        self.certainty_text = tkinter.Label(self.certainty_frame, self.style, font=('Helvetica', 9, 'bold'),
+                                            text=self.certainty)
+        self.certainty_label.pack(side='left')
+        self.certainty_text.pack(side='left')
 
-            self.certainty_label = tkinter.Label(self.certainty_frame, self.style, text="Certainty: ")
-            self.certainty_text = tkinter.Label(self.certainty_frame, self.style, font=('Helvetica', 9, 'bold'),
-                                                text=self.certainty)
-            self.certainty_label.pack(side='left')
-            self.certainty_text.pack(side='left')
+        self.delta_label = tkinter.Label(self.delta_frame, self.style, text=self.delta_msg)
+        self.delta_text = tkinter.Label(self.delta_frame, self.style, text=self.delta,
+                                        font=('Helvetica', 9, 'bold'))
+        self.delta_label.pack(side='left')
+        self.delta_text.pack(side='left')
+        self.severity_label = tkinter.Label(self.severity_frame, text="Severity: ")
+        self.severity_text = tkinter.Label(self.severity_frame, self.style, font=('Helvetica', 9, 'bold'),
+                                           text=self.severity)
 
-            self.delta_label = tkinter.Label(self.delta_frame, self.style, text=self.delta_msg)
-            self.delta_text = tkinter.Label(self.delta_frame, self.style, text=self.delta,
-                                            font=('Helvetica', 9, 'bold'))
-            self.delta_label.pack(side='left')
-            self.delta_text.pack(side='left')
-            self.severity_label = tkinter.Label(self.severity_frame, text="Severity: ")
-            self.severity_text = tkinter.Label(self.severity_frame, self.style, font=('Helvetica', 9, 'bold'),
-                                               text=self.severity)
+        self.headline_label.bind('<Button-1>', self.display_extra)
 
-            self.headline.bind('<Button-1>', self.display_extra)
+        # secondary
+        self.secondary = tkinter.Frame(self.frame, **dimensions, relief=tkinter.SUNKEN, borderwidth=4)
 
-            # secondary
-            self.secondary = tkinter.Frame(self.frame, **dimensions, relief=tkinter.SUNKEN,
-                                           borderwidth=4)
+        self.dates_frame = tkinter.Frame(self.secondary)
+        self.issued_text = tkinter.Label(self.dates_frame, text='Issued: ')
+        self.issued_time = tkinter.Label(self.dates_frame, text=format_time(self.issued),
+                                         font=('Helvetica', 9, 'bold'))
+        self.expiry_text = tkinter.Label(self.dates_frame, text='Expiry: ')
+        self.expiry_time = tkinter.Label(self.dates_frame, text=format_time(self.expiry),
+                                         font=('Helvetica', 9, 'bold'))
 
-            self.dates_frame = tkinter.Frame(self.secondary)
-            self.issued_text = tkinter.Label(self.dates_frame, text='Issued: ')
-            self.issued_time = tkinter.Label(self.dates_frame, text=format_time(json_data['issued']),
-                                             font=('Helvetica', 9, 'bold'))
-            self.expiry_text = tkinter.Label(self.dates_frame, text='Expiry: ')
-            self.expiry_time = tkinter.Label(self.dates_frame, text=format_time(json_data['expiry']),
-                                             font=('Helvetica', 9, 'bold'))
+        self.dates_frame.pack(side='top')
+        self.issued_text.pack(side='left')
+        self.issued_time.pack(side='left')
+        self.expiry_text.pack(side='left')
+        self.expiry_time.pack(side='left')
 
-            self.dates_frame.pack(side='top')
-            self.issued_text.pack(side='left')
-            self.issued_time.pack(side='left')
-            self.expiry_text.pack(side='left')
-            self.expiry_time.pack(side='left')
+        self.desclabel = tkinter.Label(self.secondary, text=self.format_description(),
 
-            self.desclabel = tkinter.Label(self.secondary, text=self.format_description(),
-                                           # level_to_style[self.level],
-                                           height=4)
-            self.desclabel.pack(side='bottom')
-            self.secondary.visible = False
+                                       height=4)
+        self.desclabel.pack(side='bottom')
+        self.secondary.visible = False
 
-    def delete(self=None, all=False):
-        if all is True:
+    def delete(self=None, del_all=False):
+        if del_all is True:
             # delete all Warning objects
             for obj in warnings_objects:
                 for widget in obj.frame.winfo_children():
@@ -220,12 +161,16 @@ class Warnings:
     def format_description(self):
         return textwrap.TextWrapper(max_lines=3).fill(text=self.description)
 
+    def create_non_warning(self, message):
+        self.frame.configure(height=10, width=60)
+        self.headline_label = tkinter.Label(self.frame, style_no_warning,
+                                            text=message)
+        self.headline_label.pack(fill='both', expand=True)
 
 def download_json(selected_region):
     if selected_region == "Demo":
-        f = open("/Users/Sniff/Desktop/warning_IRELAND.json", "r")
-        contents = f.read()
-        response = json.loads(contents)
+        with open(getcwd() + '\\' + "demo_weather_warning.json", "r") as file:
+            response = json.loads(file.read())
     else:
         if selected_region in REGION_CODES:
             areacode = REGION_CODES[selected_region]
@@ -238,12 +183,16 @@ def download_json(selected_region):
 
 
 def format_time(date_input):
+    """format time to be displayed on warning card
+    Example: 2021-02-21T14:45:06-00:00 -> Feb 21 At 14:45"""
     date_object = datetime.strptime(date_input, '%Y-%m-%dT%H:%M:%S%z')
-    # example: '2021-02-21T14:45:06-00:00 -> ' # todo fill in example
     return date_object.strftime('%b %d At %H:%M')
 
 
 def update_time():
+    """For every warning object, compare current time to expiration time to check if warning is expired.
+    If expired, return 'expired' and delete the warning card. Otherwise check if warning is in place or not in place yet
+    and assign time delta and correct label to the object attribute"""
     t_format = '%Y-%m-%dT%H:%M:%S'
     current_time = datetime.now()
 
@@ -272,32 +221,34 @@ def update_time():
 
 
 def flash_red():
+    """Make the headline of warning card flash red and white"""
     for obj in warnings_objects:
         try:
             if obj.level == "Red":
-                if obj.headline["background"] != 'white':
-                    obj.headline.configure(backgroun='white')
+                if obj.headline_label["background"] != 'white':
+                    obj.headline_label.configure(backgroun='white')
                 else:
-                    obj.headline.configure(background='red')
+                    obj.headline_label.configure(background='red')
         except AttributeError:
             pass
 
 
 def refresh(event=None):
-    Warnings.delete(all=True)
+    Warnings.delete(del_all=True)
 
     selected_region = combox.get()  # gets the selected region from combo box
     response = download_json(selected_region)
 
-    events = len(response)
-
-    # if there are no warnings, make object with None data
-    if events == 0:
-        Warnings(json_data=None)
+    # if there are no warnings, display it as a message
+    if len(response) == 0:
+        Warnings().create_non_warning(message="There are no warnings for the selected region")
     # root.geometry('350x200')
     else:
-        # root.geometry('350x' + str(200 * events + 46))
         create_object(response, selected_region)
+
+def error_no_internet():
+    # todo
+    pass
 
 
 def create_object(response, selected_region):
@@ -312,7 +263,7 @@ def create_object(response, selected_region):
 
 
 def update_combox_val():
-    sea_val = sea_box_val.get()  # include marine warnings?
+    sea_val = sea_box_val.get()
     if sea_val == 1:
         combox.configure(values=[i for i in MARINE_CODES.keys()])
         location_label.configure(text="Select Marine Zone: ")
@@ -323,7 +274,6 @@ def update_combox_val():
 
 # GUI
 root = tkinter.Tk()
-root.resizable(False, False)
 root.resizable(False, False)
 root.title("Met Eireann Status")
 root.iconbitmap("Assets/MetEireann logo-02.ico")
@@ -366,6 +316,6 @@ root.mainloop()
 # todo place icons for warning type
 # todo create a new tab showing a map
 # todo make background light gray
-# todo warning cannot connect to internet
+# todo "warning: cannot connect to internet"
 # todo give credit to met eireann
-# give region by province instead of county
+# todo give region by province instead of county
